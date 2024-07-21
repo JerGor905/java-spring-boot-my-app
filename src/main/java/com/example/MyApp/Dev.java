bf595ab2dca4fac5a11b4684d0d9c1d3f92b3d4e
Jeffery Tse <kytseag@ust.hk>
2024-07-21 23:22:02 +0800

    feat: Change to use ApplicationContext and Bean

--- src/main/java/com/example/MyApp/Dev.java
+++ src/main/java/com/example/MyApp/Dev.java
@@ -1,5 +1,8 @@
 package com.example.MyApp;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dev {
     public void build() {
         System.out.println("Working on an AWESOME project!");
--- src/main/java/com/example/MyApp/MyAppApplication.java
+++ src/main/java/com/example/MyApp/MyAppApplication.java
@@ -2,14 +2,15 @@ package com.example.MyApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MyAppApplication.class, args);
 
-		Dev obj = new Dev();
+		Dev obj = context.getBean(Dev.class);
 		obj.build();
 	}
 
